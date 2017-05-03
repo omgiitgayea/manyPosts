2795ff90dd3f2cd553b8efe95c27ca0320e22973
Christopher Rescorla <cgrescorla@gmail.com>
2017-05-02 18:07:13 -0600

    trying jspdf, may want to switch...

--- src/app/video-listing/video-listing.component.ts
+++ src/app/video-listing/video-listing.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Subscription} from "rxjs";
 import {FbServiceService} from "../fb-service.service";
+import * as jsPDF from "jspdf";
 
 @Component({
     selector: 'app-video-listing',
@@ -10,7 +11,7 @@ import {FbServiceService} from "../fb-service.service";
 export class VideoListingComponent implements OnInit {
     pageName: string;
     pageVideos: any[] = [];
-
+    LINK_SPACING = 5;
     videoPageSubscription: Subscription;
 
     constructor(private _fbService: FbServiceService) {
@@ -25,5 +26,20 @@ export class VideoListingComponent implements OnInit {
         this.pageName = this._fbService.currentName;
     }
 
+    testPdf(): void {
+        let doc = new jsPDF();
+        let position = 0;
+        doc.setFontSize(20);
+        doc.text(20, 20, this.pageName);
+        doc.setFontSize(10);
+        for(let i = 0; i < this.pageVideos.length; i++) {
+            doc.text(this.pageVideos[i].thumbnails.data[0].uri, 20, 30 + this.LINK_SPACING * position);
+            position++;
+            doc.text(`https://www.facebook.com${this.pageVideos[i].permalink_url}`, 20, 30 + this.LINK_SPACING * position);
+            position++;
+        }
 
+        // Save the PDF
+        doc.save(`${this.pageName}.pdf`);
+    }
 }
